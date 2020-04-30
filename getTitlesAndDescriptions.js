c8c354fd1ec123af209413dd7a86a27309c360f1
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-04-30 22:26:09 +0300

    Update getTitlesAndDescriptions.js

--- getTitlesAndDescriptions.js
+++ getTitlesAndDescriptions.js
@@ -3,18 +3,22 @@ function scrollToLastVideo(){
 	if(document.getElementById("spinner")==null) {clearInterval(intervalVar); getLinks();}
 } var intervalVar=setInterval(scrollToLastVideo,10);
 
+var str="";
 function getLinks() {
-	var str="", i=0, noVideos=document.getElementsByTagName("ytd-grid-video-renderer").length, x=noVideos;
+	var noVideos=document.getElementsByTagName("ytd-grid-video-renderer").length, x=noVideos-1, i=1;
 	var iFrame=document.createElement("iframe"); iFrame.id="iFrame"; document.body.appendChild(iFrame);
+	iFrame.src=document.getElementsByTagName("ytd-grid-video-renderer")[x].childNodes[1].childNodes[0].childNodes[2].href;
 	function getTitlesAndDescriptions() {
 		str+="xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; for(var y=i.toString().length; y<noVideos.toString().length; y++) {str+="0"}; str+=i+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxx|<br>"+
 		document.getElementById("iFrame").contentWindow.document.getElementsByTagName("h1")[0].textContent+"<br>"+
 		document.getElementById("iFrame").contentWindow.document.getElementsByClassName("view-count")[0].textContent+document.getElementById("iFrame").contentWindow.document.getElementById("date").textContent+"<br>"+
 		"----------------------------------------<br>"+
 		document.getElementById("iFrame").contentWindow.document.getElementById("description").textContent+"<br>";
-		x--; i++; iFrame.src=document.getElementsByTagName("ytd-grid-video-renderer")[x].childNodes[1].childNodes[0].childNodes[2].href;
-		if (x==-1) {clearInterval(intervalVar); display();}
+		if (x==0) {clearInterval(intervalVar); display();}
+		x--; i++;
+		iFrame.src=document.getElementsByTagName("ytd-grid-video-renderer")[x].childNodes[1].childNodes[0].childNodes[2].href;
 	} var intervalVar=setInterval(getTitlesAndDescriptions,5000);
 	
 }
+
 function display() {document.write(str); document.body.style.fontFamily="Lucida Console, monospace";}
