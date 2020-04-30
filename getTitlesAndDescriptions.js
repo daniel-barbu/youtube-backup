bbf2d2bceedf61c08ff65c77b06e4e7ddd8b8b28
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-04-30 20:58:44 +0300

    Update getTitlesAndDescriptions.js

--- getTitlesAndDescriptions.js
+++ getTitlesAndDescriptions.js
@@ -5,12 +5,14 @@ function scrollToLastVideo(){
 
 function getLinks() {
 	var str="", urlTemp="", i=0; noVideos=document.getElementsByTagName("ytd-grid-video-renderer").length;
-	for (var x=noVideos-1; x>=0; x--) {i++; urlTemp=document.getElementsByTagName("ytd-grid-video-renderer")[x].childNodes[1].childNodes[0].childNodes[2].href;
+	var iFrame=document.createElement("iframe"); iFrame.id="iFrame"; document.body.appendChild(iFrame);
+	for (var x=noVideos-1; x>=0; x--) {i++; iFrame.src=document.getElementsByTagName("ytd-grid-video-renderer")[x].childNodes[1].childNodes[0].childNodes[2].href;
+					   //the page is loading...
                                                 str+="xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; for(var y=i.toString().length; y<noVideos.toString().length; y++) {str+="0"}; str+=i+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxx|<br>"+
-                                                /*titlu*/+"<br>"+
-                                                /*vizionari, data*/+"<br>"+
+                                                document.getElementById("iFrame").contentWindow.document.getElementsByTagName("h1")[0].textContent+"<br>"+
+                                                document.getElementById("iFrame").contentWindow.document.getElementsByClassName("view-count")[0].textContent+document.getElementById("iFrame").contentWindow.document.getElementById("date").textContent+"<br>"+
                                                 "----------------------------------------<br>"+
-                                                /*descriere*/+"<br>";
+                                                "descriere"+"<br>";
                                           }
-          document.write(str); document.body.style.fontFamily="Lucida Console, monospace";
+	document.write(str); document.body.style.fontFamily="Lucida Console, monospace";
 }
